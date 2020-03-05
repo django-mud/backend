5df6fbd5372649ad1f4de80a9cf300195398e6eb
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2020-03-05 12:30:12 -0800

    add current room id to move endpoint

--- adventure/api.py
+++ adventure/api.py
@@ -58,7 +58,7 @@ def move(request):
         return JsonResponse({'name':player.user.username, 'title':nextRoom.title, 'description':nextRoom.description, 'players':players, 'error_msg':""}, safe=True)
     else:
         players = room.playerNames(player_id)
-        return JsonResponse({'name':player.user.username, 'title':room.title, 'description':room.description, 'players':players, 'error_msg':"You cannot move that way."}, safe=True)
+        return JsonResponse({'room_id': room.id, 'name':player.user.username, 'title':room.title, 'description':room.description, 'players':players, 'error_msg':"You cannot move that way."}, safe=True)
 
 
 @csrf_exempt
