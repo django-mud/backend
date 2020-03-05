f624d338da14880ab820993a4710a0e7e7a1df2a
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2020-03-04 23:13:26 -0800

    update create world

--- util/create_world.py
+++ util/create_world.py
@@ -1,26 +1,18 @@
 from django.contrib.auth.models import User
 from adventure.models import Player, Room
-
-
 Room.objects.all().delete()
-
 r_outside = Room(title="Outside Cave Entrance",
                description="North of you, the cave mount beckons", x=0, y=0)
-
 r_foyer = Room(title="Foyer", description="""Dim light filters in from the south. Dusty
 passages run north and east.""", x=0, y=1)
-
 r_overlook = Room(title="Grand Overlook", description="""A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm.""", x=0, y=2)
-
 r_narrow = Room(title="Narrow Passage", description="""The narrow passage bends here from west
 to north. The smell of gold permeates the air.""", x=1, y=2)
-
 r_treasure = Room(title="Treasure Chamber", description="""You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south.""", x=1, y=3)
-
 r_outside.save()
 r_foyer.save()
 r_overlook.save()
@@ -30,16 +22,12 @@ r_treasure.save()
 # Link rooms together
 r_outside.connectRooms(r_foyer, "n")
 r_foyer.connectRooms(r_outside, "s")
-
 r_foyer.connectRooms(r_overlook, "n")
 r_overlook.connectRooms(r_foyer, "s")
-
 r_foyer.connectRooms(r_narrow, "e")
 r_narrow.connectRooms(r_foyer, "w")
-
 r_narrow.connectRooms(r_treasure, "n")
 r_treasure.connectRooms(r_narrow, "s")
-
 players=Player.objects.all()
 for p in players:
   p.currentRoom=r_outside.id
